aed9a518bdadb75ebc38036e3e937c7b68593892
gksouza <gkoliver2020@gmail.com>
2024-02-17 14:48:52 -0300

    css in js

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,11 +1,11 @@
-import "./styles.css"
+import { Container } from "./styles";
 
 export function Details() {
 
   return(
-    <>
+    <Container>
       <h1>Grace Kelly</h1>
       <span>ótima de excelente!</span>
-    </>    
+    </Container>    
   )
 }
