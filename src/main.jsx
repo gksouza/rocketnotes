5b3e0b1049256451e6fbc8d529f935b791bfcd7d
gksouza <gkoliver2020@gmail.com>
2024-02-17 17:59:03 -0300

    learning about global styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Details } from './pages/Details'
-import { ThemeProvider } from './styles-components'
+import { ThemeProvider } from 'styled-components'
+import GlobalStyles from './styles/global'
 import theme from './styles/theme'
+import { Details } from './pages/Details'
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <Details/>
+      <GlobalStyles />
+      <Details />
     </ThemeProvider>        
   </React.StrictMode>,
 )
