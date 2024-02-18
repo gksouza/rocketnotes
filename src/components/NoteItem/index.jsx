6e32d3c5b543af253314a1d2aab44aaa1e073819
gksouza <gkoliver2020@gmail.com>
2024-02-18 19:47:05 -0300

    styling the noteitem

--- src/components/NoteItem/index.jsx
+++ src/components/NoteItem/index.jsx
@@ -1,4 +1,4 @@
-import { FiPlus, Fix } from 'react-icons/fi'
+import { FiPlus, FiX } from 'react-icons/fi'
 
 import { Container } from './styles';
 
@@ -16,8 +16,9 @@ export function NoteItem({ isNew, value, onClick, ...rest }) {
       <button
       type="button"
         onClick={onClick}
+        className={isNew ? 'button-add' : 'button-delete'}
       >
-        { isNew ? <FiPlus /> : <Fix />}
+        { isNew ? <FiPlus /> : <FiX /> }
       </button>
     </Container>
   );
--- src/components/NoteItem/styles.js
+++ src/components/NoteItem/styles.js
@@ -1,26 +1,44 @@
 import styled from 'styled-components';
 
-export const Container = styled.button`
-  width: 100%;
-  background-color: ${({ theme }) => theme.COLORS.BACKGROUND_700};
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
 
-  border: none;
+  background-color: ${({ theme, isNew }) => isNew ? "transparent" : theme.COLORS.BACKGROUND_900};
+  color: ${({ theme, isNew }) => theme.COLORS.GRAY_300};
+
+  border: ${({ theme, isNew }) => isNew ? `1px dashed ${theme.COLORS.GRAY_300}` : "none"};
+
+  margin-bottom: 8px;
   border-radius: 10px;
+  padding-right: 10px;
 
-  padding: 22px;
-  margin-bottom: 16px;
-
-  > h1 {
-    flex: 1;
-    text-align: left;
-    font-weight: 700;
-    font-size: 24px;
-    color: ${({ theme }) => theme.COLORS.WHITE};
+  > button {
+    border: none;
+    background: none;
   }
 
-  > footer {
+  .button-delete {
+    color: ${({ theme }) => theme.COLORS.RED};
+  }
+
+  .button-add {
+    color: ${({ theme }) => theme.COLORS.ORANGE};
+  }
+
+  > input {
+    height: 56px;
     width: 100%;
-    display: flex;
-    margin-top: 24px;
-  }
+
+    padding: 12px;
+
+    color: ${({ theme }) => theme.COLORS.WHITE};
+    background: transparent;
+
+    border: none;
+
+    &::placeholder {
+      color: ${({ theme }) => theme.COLORS.GRAY_300};
+    }
+  }  
 `;
