6516a8d9d3beacaecbf1c6fa19eead00cb46b0cd
gksouza <gkoliver2020@gmail.com>
2024-02-18 08:48:52 -0300

    created button text

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,14 +1,19 @@
 import { Container, Links } from "./styles";
-import { Header } from '../../components/Header';
-import { Button } from '../../components/Button';
-import { Section } from '../../components/Section';
+
 import { Tag } from '../../components/Tag';
+import { Button } from '../../components/Button';
+import { Header } from '../../components/Header';
+import { Section } from '../../components/Section';
+import { ButtonText } from '../../components/ButtonText';
 
 export function Details() {
 
   return(
     <Container>
       <Header />
+
+      <ButtonText title="Excluir nota" />
+
       <Section title="Links úteis">
         <Links>
           <li><a href="#">https://www.rocketseat.com.br/</a></li>
@@ -19,7 +24,7 @@ export function Details() {
       <Section title="Marcadores">
        <Tag title="express"/>
        <Tag title="nodejs"/>
-      </Section>
+      </Section>     
       
       <Button title="Voltar"/>
     </Container>    
