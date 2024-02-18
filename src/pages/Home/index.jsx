5f4e1218dc528491ad952115511abf8dcd7b4e9d
gksouza <gkoliver2020@gmail.com>
2024-02-18 14:21:18 -0300

    created notes component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,8 +2,10 @@ import { FiPlus, FiSearch } from 'react-icons/fi'
 
 import { Container, Brand, Menu, Search, Content, NewNote } from "./styles";
 
-import { Header } from '../../components/Header'
+import { Note } from '../../components/Note'
 import { Input } from '../../components/Input'
+import { Header } from '../../components/Header'
+import { Section } from '../../components/Section'
 import { ButtonText } from '../../components/ButtonText'
 
 
@@ -26,7 +28,17 @@ export function Home() {
         <Input placeholder="Pesquisar pelo título" icon={FiSearch}  />
       </Search>
 
-      <Content>        
+      <Content>
+        <Section title="Minhas notas">
+          <Note data={{
+            title: 'React',
+            tags: [
+              { id: '1', name: 'react' },
+              { id: '2', name: 'rocketseat' }
+            ]
+          }}
+          />
+        </Section>
       </Content>
 
       <NewNote>
