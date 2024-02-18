251b1ee2f7ceec105dfba5375da0040ebf132322
gksouza <gkoliver2020@gmail.com>
2024-02-18 08:23:44 -0300

    created tag component

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -2,6 +2,7 @@ import { Container, Links } from "./styles";
 import { Header } from '../../components/Header';
 import { Button } from '../../components/Button';
 import { Section } from '../../components/Section';
+import { Tag } from '../../components/Tag';
 
 export function Details() {
 
@@ -14,6 +15,12 @@ export function Details() {
           <li><a href="#">https://www.rocketseat.com.br/</a></li>      
         </Links>
       </Section>
+
+      <Section title="Marcadores">
+       <Tag title="express"/>
+       <Tag title="nodejs"/>
+      </Section>
+      
       <Button title="Voltar"/>
     </Container>    
   )
