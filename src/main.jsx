84c821fd5a39947fb9303521fe1a83d926fe6cf4
gksouza <gkoliver2020@gmail.com>
2024-02-18 17:06:44 -0300

    created signin structure

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom/client'
 import { ThemeProvider } from 'styled-components'
 import GlobalStyles from './styles/global'
 import theme from './styles/theme'
-import { Home } from './pages/Home'
+// import { Home } from './pages/Home'
+import { SignIn } from './pages/Signin'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Home />
+      {/* <Home /> */}
+      <SignIn />
     </ThemeProvider>        
   </React.StrictMode>,
 )
