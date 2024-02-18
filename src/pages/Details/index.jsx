10f0b9857761a72e8eb4736e144f41b5a0785b64
gksouza <gkoliver2020@gmail.com>
2024-02-18 07:06:51 -0300

    created header component

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,15 +1,12 @@
 import { Container } from "./styles";
+import { Header } from '../../components/Header';
 import { Button } from '../../components/Button';
 
 export function Details() {
 
   return(
     <Container>
-      <h1>Grace Kelly</h1>
-      <span>ótima de excelente!</span>
-
-      <Button title="Login" loading/>
-      <Button title="Cadastrar"/>
+      <Header />
       <Button title="Voltar"/>
     </Container>    
   )
