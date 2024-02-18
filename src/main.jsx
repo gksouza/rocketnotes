335907dff5c5cc623ead9878bbbcaf3aa8599574
gksouza <gkoliver2020@gmail.com>
2024-02-18 17:58:06 -0300

    created created signup screen

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,14 @@ import { ThemeProvider } from 'styled-components'
 import GlobalStyles from './styles/global'
 import theme from './styles/theme'
 // import { Home } from './pages/Home'
-import { SignIn } from './pages/Signin'
+import { SignUp } from './pages/SignUp'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
       {/* <Home /> */}
-      <SignIn />
+      <SignUp />
     </ThemeProvider>        
   </React.StrictMode>,
 )
