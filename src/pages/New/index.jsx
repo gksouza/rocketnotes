6dd7c808edd632f85ffad0186bd488826f95603b
gksouza <gkoliver2020@gmail.com>
2024-02-18 19:57:23 -0300

    finishing the new

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -2,7 +2,8 @@ import { Header } from '../../components/Header'
 import { Input } from '../../components/Input'
 import { Textarea } from '../../components/Textarea'
 import { NoteItem } from '../../components/NoteItem'
-import { Section} from '../../components/Section'
+import { Section } from '../../components/Section'
+import { Button } from '../../components/Button'
 
 import { Container, Form } from "./styles";
 
@@ -25,6 +26,15 @@ export function New() {
             <NoteItem value="https://rocketseat.com.br" />
             <NoteItem isNew placeholder="Novo link" />
           </Section>
+
+          <Section title="Marcadores">
+            <div className='tags'>
+            <NoteItem value="react" />
+            <NoteItem isNew placeholder="Nova tag" />
+            </div>
+          </Section>
+
+          <Button title="Salvar" />
         </Form>
       </main>    
     </Container>    
