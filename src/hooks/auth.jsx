cfa89a073e80dc5794dcef834e71b4cc865a5bf8
gksouza <gkoliver2020@gmail.com>
2024-02-26 09:14:43 -0300

    adjustment in request header

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -16,9 +16,9 @@ function AuthProvider({ children }) {
       localStorage.setItem("@rocketnotes:user", JSON.stringify(user));
       localStorage.setItem("@rocketnotes:token", token);
       
-      api.defaults.headers.authorization = 'Bearer ${token}';
-      setData({ user, token })
-              
+      api.defaults.headers.common['Authorization'] = 'Bearer ${token}';
+
+      setData({ user, token })              
     } catch(error) {
       if(error.response) {
         alert(error.response.data.message)
@@ -40,8 +40,8 @@ function AuthProvider({ children }) {
     const user = localStorage.getItem("@rocketnotes:user");
 
     if(token && user) {
-      api.defaults.headers.authorization = 'Bearer ${token}';
-
+      api.defaults.headers.common['Authorization'] = 'Bearer ${token}';
+      
       setData ({
         token,
         user: JSON.parse(user)
